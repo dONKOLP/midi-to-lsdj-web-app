94ebf6dfd220f98c5778b832c538acde10e5f2db
dONKOLP <96030212+dONKOLP@users.noreply.github.com>
2026-06-05 11:18:28 +1000

    Include tempo in track data response

--- pages/api/transcribe.ts
+++ pages/api/transcribe.ts
@@ -28,7 +28,7 @@ export default async function handler(req, res) {
         const trackData = processTrack(midiData, 0)
         const projectData = processProject(midiData)
         res.status(200).json({
-          track: trackData,
+          track: { ...trackData, tempo: projectData.tempo },
           project: projectData
         })
       }
